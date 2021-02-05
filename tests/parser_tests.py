86de2a6d031c7fd9d0a0d237a9d709caec64b3ec
Olexandr Hutsal <oahutsal@gmail.com>
2021-02-05 18:48:33 +0200

    Restructured (closing #3)

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -1,1 +1,9 @@
-print('Hello world!')
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_title_type(self):
+      self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
